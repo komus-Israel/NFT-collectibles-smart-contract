46d38435c47676588bc80057dd4d7fd3e525c997
komus-Israel <komolehinIsrael@gmail.com>
2021-12-21 14:16:08 +0100

    deploy the contract using beforeEach before running any instanc e of the unit Test

--- test/Nft.test.js
+++ test/Nft.test.js
@@ -10,17 +10,31 @@ const NftContract = artifacts.require("./NFT.sol")
 contract('NFT', ([account1])=>{
 
     let contract;
-    describe("deployent", ()=>{
 
+    beforeEach(async()=>{
+
+        // deploy the contract before starting each instacnce of the test   
+        contract = await NftContract.deployed()
+        
+    })
+
+
+    describe("deployent", ()=>{
+    
         it("deploys successfully", async()=>{
-            contract = await NftContract.deployed()
+            
             
             const contractAddress = contract.address 
-            //contractAddress.should.not.equal(account1)
-            //contractAddress.should.not.equal('')
-
-            assert.notEqual(contractAddress, '')
+            contractAddress.should.not.equal(account1)
+            contractAddress.should.not.equal('')  
            
         })
+
+       /* it("has a name", async()=>{
+            const nftName = contract.name()
+            nftName.should.not.equal('')
+        })*/
     })
+
+    
 })
